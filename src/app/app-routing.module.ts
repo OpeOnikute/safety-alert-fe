d117d1678f84800b55b866005f351f6abb26d2d1
opeyemi <opeyemionikute@yahoo.com>
2018-07-02 08:48:30 +0100

    initial commit 2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MainComponent } from './main/main.component';
+import { IndexComponent } from './index/index.component';
+import { EntriesComponent } from './entries/entries.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent,
+    children: [
+      { path: 'index', component: IndexComponent },
+      { path: 'entries', component: EntriesComponent },
+      { path: 'about', component: AboutComponent },
+      { path: '**', redirectTo: 'index' }
+    ]},
+  { path: '**', redirectTo: '' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { IndexComponent } from './index/index.component';
+import { EntriesComponent } from './entries/entries.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    IndexComponent,
+    EntriesComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
