62804d77952d24c10aa04c225072758cbb634bd9
opeyemi <opeyemionikute@yahoo.com>
2018-07-08 19:53:40 +0100

    minor fix

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/safety-alert-fe'));
+app.use(express.static(__dirname + '/dist/safety-alert'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/safety-alert-fe/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/safety-alert/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
