83e58509ef50b1f3aec92926438e1f33e8195fb8
opeyemi <opeyemionikute@yahoo.com>
2018-07-29 01:18:05 +0100

    fix: move subscribe logic into about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
+import { SwPush } from '@angular/service-worker';
+import { MessageService } from '../../services/message/message.service';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-about',
@@ -8,13 +11,30 @@ import { AppComponent } from '../app.component';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private app: AppComponent) { }
+  readonly VAPID_PUBLIC_KEY = 'BPzdlyQsqF_xFKTL28WM609TPJa_YYXFzdWuhFtt9h_wkQ-Bl-oOhO2ImRoqQQc9ypX3CT5lWBdd-uw4jv5sZ-E';
+
+  constructor(private app: AppComponent, private swPush: SwPush, private messaging: MessageService,
+          private userService: UserService) {}
 
   ngOnInit() {
     this.app.setTitle('About');
   }
 
   subscribe() {
-    this.app.subscribeToNotifications();
+    this.swPush.requestSubscription({
+      serverPublicKey: this.VAPID_PUBLIC_KEY
+    })
+      .then(sub => {
+        this.userService.createPush(sub).then(data => {
+          this.messaging.sendMessage('success', 'You Rock! You have been subscribed successfully.');
+        }, err => {
+          this.messaging.sendMessage('error', err || 'Technical issue: An error occurred. Please try again.');
+        });
+        console.log(sub);
+      })
+      .catch(err => {
+        this.messaging.sendMessage('error', err || 'Technical issue: An error occurred. Please try again.');
+        console.error('Could not subscribe to notifications', err);
+      });
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { SwPush } from '@angular/service-worker';
-import { MessageService } from '../services/message/message.service';
 import { LogUpdateService } from '../services/log-update.service';
-import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-root',
@@ -12,32 +9,9 @@ import { UserService } from '../services/user/user.service';
 })
 export class AppComponent {
 
-  readonly VAPID_PUBLIC_KEY = 'BPzdlyQsqF_xFKTL28WM609TPJa_YYXFzdWuhFtt9h_wkQ-Bl-oOhO2ImRoqQQc9ypX3CT5lWBdd-uw4jv5sZ-E';
-
-  constructor(private swPush: SwPush, private titleService: Title, private messaging: MessageService,
-              log: LogUpdateService, private userService: UserService) {}
+  constructor(private titleService: Title, log: LogUpdateService) {}
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle(`Safety Alert | ${newTitle}`);
-  }
-
-  public subscribeToNotifications() {
-
-    this.swPush.requestSubscription({
-      serverPublicKey: this.VAPID_PUBLIC_KEY
-    })
-      .then(sub => {
-        this.userService.createPush(sub).then(data => {
-          this.messaging.sendMessage('success', 'You Rock! You have been subscribed successfully.');
-        }, err => {
-          this.messaging.sendMessage('error', err || 'Technical issue: An error occurred. Please try again.');
-        });
-        console.log(sub);
-      })
-      .catch(err => {
-        this.messaging.sendMessage('error', err || 'Technical issue: An error occurred. Please try again.');
-        console.error('Could not subscribe to notifications', err);
-      });
-
   }
 }
